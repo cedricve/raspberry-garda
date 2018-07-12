37d97a1d209680c11a78b12d7cb2fa79021d0cba
ChieftainY2k <ChieftainY2k@gmail.com>
2018-07-12 19:28:50 +0200

    Media browser container defs

--- services/mediabrowser/run.sh
+++ services/mediabrowser/run.sh
@@ -1,13 +1,9 @@
 
-# run the simple PHP process to act as web interface
-while sleep 3; do
-    echo "Starting the configurator server..."
-    php -S 0.0.0.0:80 KodExplorer/index.php
-done
+echo "Starting media browser service."
 
+service php7.0-fpm restart
+service nginx restart
 
-#sleep infinity
-
-
+sleep infinity
 
 
