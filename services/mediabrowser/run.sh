141d1224704cf03b005d092c8837fb2f0059194d
ChieftainY2k <ChieftainY2k@gmail.com>
2018-07-12 18:55:56 +0200

    media browser container stub

--- services/mediabrowser/run.sh
+++ services/mediabrowser/run.sh
@@ -1,5 +1,12 @@
 
-sleep infinity
+# run the simple PHP process to act as web interface
+while sleep 3; do
+    echo "Starting the configurator server..."
+    php -S 0.0.0.0:80 KodExplorer/index.php
+done
+
+
+#sleep infinity
 
 
 
